9712ce868d8cd318f1d53ecbe0b6160ff62d5572
Dongsu Jang <iolothebard@gmail.com>
2019-07-04 09:11:43 +0900

    cleanup sample vue app

--- src/main.js
+++ src/main.js
@@ -4,30 +4,16 @@
 import Vue from 'vue';
 import Composer from './views/composer.vue';
 
-// sample layouts with es6 module:
-import layouts from '../public/layouts/src/all';
-import '../public/layouts/src/all.css';
-
-// sample layouts with commonjs bundle
-// need to build with `npm run build-layouts` or else
-// import layouts from '../public/layouts/all.json';
-// import '../public/layouts/all.css';
-
-Vue.config.productionTip = false;
-
-const el = document.querySelector('#app');
-
-// sample layouts with commonjs bundle:
-// FIXME: resolve icons automatically...
-// layouts.forEach(layout => {
-//   layout.icon = require(`../public/layouts/${layout.id}/icon.svg`);
-// });
+import layouts from '../layouts';
+import sample from '../layouts/sample.json';
 
 new Vue({
-  el,
   render(createElement) {
     return createElement(Composer, {
-      ref: 'composer',
+      props: {
+        layerModals: sample,
+        layoutModels: layouts,
+      },
       on: {
         save(html, json) {
           console.log('**save: html=', html, 'json=', json);
@@ -36,17 +22,4 @@ new Vue({
       },
     });
   },
-  mounted() {
-    this.$refs.composer.setLayouts(layouts);
-  },
-});
-/**
- * vue 라이프사이클에서 layout과 data를 구성하기보단 밖에서 별도로 분리하여 설정
- */
-// axios.get('/sample.json').then((resources) => {
-//   app.$refs.composer.setLayerBlockData(resources.data);
-// });
-
-// console.log(layouts); // 실제 layout 도구들...
-// console.log(res.data); // layouts들을 이용하여 data와 결합하여 preview에 보여준다
-
+}).$mount('#app');
