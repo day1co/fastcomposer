6e53b4b291eaf05f65bbb16af5f99d8d51f61507
Jaewon-Noh <jwno@fastcampus.co.kr>
2021-01-06 11:45:10 +0900

    eslintrc 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,16 @@ module.exports = {
     node: true,
     mocha: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: 'babel-eslint',
+    ecmaVersion: 9,
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+  },
+};
