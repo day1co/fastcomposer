3f4037aaef80a50816ece4d1133070775193aa18
jongyi kim (hibiya) <jikim@day1company.co.kr>
2022-07-20 18:37:59 +0900

    preview: layout id gutter

--- src/views/preview/preview.vue
+++ src/views/preview/preview.vue
@@ -10,13 +10,22 @@
        @keydown.exact.page-down.prevent="select(index + 5)"
   >
     <div class="fc-preview__container">
-      <div class="fc-block" v-for="(block, blockIndex) in blocks" :key="'block-' + blockIndex">
+      <div :class="['fc-block', { 'fc-selected': blockIndex === currentLayerIndex }]"
+           v-for="(block, blockIndex) in blocks"
+           :key="'block-' + blockIndex"
+           @click="select(blockIndex)">
+        <div class="fc-block__info">
+          <div>
+            <b>{{ block.layout.id }}</b>
+            <br />
+            #{{ blockIndex }}
+          </div>
+        </div>
         <div
           class="fc-block__container"
           :id="block.id"
-          :class="['fc-layout-' + block.layout.id, { 'fc-selected': blockIndex === currentLayerIndex }, {'fc-hidden': block.hidden} ]"
+          :class="['fc-layout-' + block.layout.id, {'fc-hidden': block.hidden} ]"
           v-html="parserToHTML(block)"
-          @click="select(blockIndex)"
         ></div>
       </div>
     </div>
@@ -79,17 +88,59 @@
   @use 'sass:math';
   @import '../../assets/scss/utils/utilities';
 
+  $accent: #f00;
+  $gutter-width: 3.2rem;
+
   .fc-preview {
     overflow: auto;
     background-color: $white;
+
+    box-shadow: $gutter-width 0 0 $primary inset, -0.4rem 0 0 $primary inset;
   }
 
   .fc-block {
     position: relative;
-    .fc-selected {
-      box-shadow: inset 0 0 0 .4rem red;
-      padding: .4rem;
+    display: grid;
+    grid-template-columns: $gutter-width auto;
+    border-right: 0.4rem solid $primary;
+
+    &.fc-selected {
+
+      .fc-block__info {
+        background: $accent;
+        &::after {
+          display: none;
+        }
+      }
+
+      &::after {
+        display: block;
+        content: '';
+        width: calc(100% - $gutter-width);
+        height: 100%;
+        position: absolute;
+        top: 0;
+        right: -0.4rem;
+        bottom: 0;
+        margin: auto;
+        border: solid #f00;
+        border-width: 0.4rem 0.4rem 0.4rem 0;
+        z-index: 1;
+        box-sizing: content-box;
+      }
     }
+    &__info {
+      position: relative;
+      background: $primary;
+      color: white;
+      font-size: 1.2rem;
+      overflow: hidden;
+      border-bottom: 0.1rem solid white;
+
+      user-select: none;
+      cursor: pointer;
+    }
+
     .fc-block__preview {
       flex: 0 0 0;
       width: 100%;
