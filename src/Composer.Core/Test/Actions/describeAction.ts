3eb211df51e24de8ccfef10c8b943ea987572e44
jongyi kim (hibiya) <jikim@day1company.co.kr>
2022-09-15 20:32:31 +0900

    core/test: cleanup; add 'deps' param to describeAction;

--- src/Composer.Core/Test/Actions/describeAction.ts
+++ src/Composer.Core/Test/Actions/describeAction.ts
@@ -7,32 +7,68 @@ jest.mock('../../Util', () => ({
 
 import { uniqueId } from '../../Util'
 
+import type Action from '../../Actions/IAction'
+
 import Act from '../../Act'
 import Actions from '../../Actions'
 import State from '../../State'
 
 import * as setup from '../setup'
 
-export default function describeAction(
+function describeAction(
   actionName: string,
   cb: (helpers: any, ...rest: any) => any
+): void
+function describeAction(
+  actionName: string,
+  dependentActions: Array<string>,
+  cb: (helpers: any, ...rest: any) => any
+): void
+
+function describeAction(
+  actionName: string,
+  dependentActions: Array<string> | ((helpers: any, ...rest: any) => any),
+  cb?: (helpers: any, ...rest: any) => any
 ) {
-  // place helpers inside closuers to (maybe)make use of local scope
+  if(!Array.isArray(dependentActions)) {
+    cb = dependentActions
+    dependentActions = []
+  }
+
+  // still can't believe this *is* most beloved programming language
+  // https://github.com/microsoft/TypeScript/issues/9998
+  const _dependentActions = dependentActions
+
   const helpers: any = {
     mocked: {
       uniqueId: <jest.Mocked<typeof uniqueId>>jest.mocked(uniqueId)
     },
-    createStateWithActionAlone(layouts = setup.MinimalLayouts) {
-      const action = Actions.get(actionName)
-      if(!action)
-        throw new ReferenceError(`test setup for '${actionName}' requested, but not found in Actions`)
+    createState(layouts = setup.MinimalLayouts) {
+      const actions = [ actionName, ..._dependentActions ]
+        .map(action => <const>[ action, Actions.get(action) ])
+      const notfound = actions
+        .filter(l => l[1] == null)
+        .map(l => l[0])
+        .join(', ')
 
-      return new State(layouts, new Map([[ actionName, action ]]))
+      if(notfound)
+        throw new ReferenceError(`following actions requested, \
+          but not found in Actions: ${notfound}`)
+
+      return new State(layouts, new Map(actions))
     },
-    createAct(...args: Omit<ConstructorParameters<typeof Act>, 0>) {
-      return new Act(actionName, ...args)
-    }
+    createAct(...args: ConstructorParameters<typeof Act> | Omit<ConstructorParameters<typeof Act>, 0>) {
+      if(typeof args[0] !== 'string') {
+        return new Act(actionName, ...args)
+      } else {
+        // ¯\_(ツ)_/¯
+        return new Act(...<ConstructorParameters<typeof Act>>args)
+      }
+
+    },
   }
 
-  describe('Action: ' + actionName, (...args) => cb(helpers, ...args))
+  return describe('Action: ' + actionName, (...args) => cb(helpers, ...args))
 }
+
+export default describeAction
