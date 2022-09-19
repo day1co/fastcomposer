1463fcf7bfb127c63f468380e1544708974b895b
jongyi kim (hibiya) <jikim@day1company.co.kr>
2022-09-19 21:22:01 +0900

    core: layer: (add|remove)ItemFor accepts Path now

--- src/Composer.Core/Layer.ts
+++ src/Composer.Core/Layer.ts
@@ -31,27 +31,31 @@ export default class Layer {
 
     return def
   }
-  addItemFor(key: string) {
-    const def = <ListLayoutParameter>this._getDef(key)
+  addItemFor({ child }: Path) {
+    const def = <ListLayoutParameter>this._getDef(child)
     if(def.type !== 'list')
       throw new TypeError('trying to add item to non-list type item')
 
-    const list = this.values[key]
+    const list = this.values[child]
     // TODO: not to throw?
     if(list.length >= def.maxLength)
-      throw new TypeError(`value '${key}' has already exceeded its maxLength`)
+      throw new TypeError(`value '${child}' has already exceeded its maxLength`)
 
-    list.push(this.layout.getDefaultValues(key))
+    list.push(this.layout.getDefaultValues(child))
   }
-  removeItemFor(key: string, at: number) {
-    const def = <ListLayoutParameter>this._getDef(key)
+  removeItemFor({ child }: Path, at?: number | string) {
+    const def = <ListLayoutParameter>this._getDef(child)
     if(def.type !== 'list')
       throw new TypeError('trying to remove item from non-list type item')
 
-    const list = this.values[key]
+    if(at === null) {
+      at = child.split('/')[1]
+    }
+
+    const list = this.values[child]
     // TODO: not to throw?
     if(list.length - 1 < at)
-      throw new TypeError(`value '${key}' has already exceeded its maxLength`)
+      throw new TypeError(`value '${child}' has no element at index ${at}`)
 
     list.slice(at, 1)
   }
