9571b15768951953bfe16b84e342b796378af21d
jongyi kim (hibiya) <jikim@day1company.co.kr>
2022-09-20 18:10:49 +0900

    core: path: partial, override

--- src/Composer.Core/Path.ts
+++ src/Composer.Core/Path.ts
@@ -12,4 +12,33 @@ export default class Path {
            this?.index === path?.index &&
            this?.grandchild === path?.grandchild
   }
+
+  partial(until: string) {
+    switch(until) {
+      case 'layer':
+        return new Path(this.layer)
+      case 'child':
+        return new Path(this.layer, this.child)
+      case 'index':
+        return new Path(this.layer, this.child, this.index)
+      case 'grandchild': // ???
+        return new Path(this.layer, this.child, this.index, this.grandchild)
+      default:
+        throw new TypeError(`invalid Path#partial option given`)
+    }
+  }
+
+  override({ layer, child, index, grandchild }) {
+    layer = layer ?? this.layer
+    child = child ?? this.child
+    if(index != null)
+      if(child == null)
+      throw new ReferenceError(`tried to set 'index' without 'child'`)
+    index = index ?? this.index
+    if(grandchild != null)
+      if(index == null || child == null)
+        throw new ReferenceError(`tried to set 'grandchild' without 'child' and/or 'index'`)
+    grandchild = grandchild ?? this.grandchild
+    return new Path(layer, child, index, grandchild)
+  }
 }
