b7ad8c97d33471820e6c2de5d7db7a828087fe9f
jongyi kim (hibiya) <jikim@day1company.co.kr>
2023-02-14 19:00:41 +0900

    layouts-example: initial commit (wip but works)

---
+++ layouts-example/index.js
@@ -0,0 +1,23 @@
+import * as ComposerLayout from '../src/layout'
+import LegacyLayout from '../src/layout/legacy'
+
+const imports = Object.values({
+  ...import.meta.glob('./*/index.js', { eager: true }),
+  ...import.meta.glob('./*/index.jsx', { eager: true }),
+  ...import.meta.glob('./*/index.ts', { eager: true }),
+  // ...import.meta.glob('./*/index.tsx', { eager: true })
+})
+
+const layouts = new Map()
+
+for(let module of imports) {
+  const layout = module.default
+
+  if(!layout._specVersion || layout._specVersion < 2) {
+    layouts.set(layout.id, LegacyLayout.fromDefinition(layout))
+  } else {
+    layouts.set(layout.id, layout)
+  }
+}
+
+export default layouts
