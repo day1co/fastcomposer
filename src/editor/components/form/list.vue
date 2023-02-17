6f9186aa916273743f3c9f231e0e958ce0f3809a
jongyi kim (hibiya) <jikim@day1company.co.kr>
2023-02-17 18:23:29 +0900

    editor/form/list: style cleanup

--- src/editor/components/form/list.vue
+++ src/editor/components/form/list.vue
@@ -1,9 +1,10 @@
 <template>
-  <ol class="composer-input-list" start="0">
+  <ol class="composer-input-list">
     <li class="composer-input-title">
       <button
         class="composer-input-button composer-input-list-button"
-        @click="addItem">
+        @click="addItem"
+        :disabled="!spaceLeft">
         <svg width="16" height="16" fill="none" stroke="currentColor">
           <path d="M4 8h8M8 4v8" />
         </svg>
@@ -46,7 +47,8 @@ export default {
   },
   methods: {
     addItem() {
-      this.$composer.state.improvise('layer.item.new', this.path)
+      if(this.spaceLeft)
+        this.$composer.state.improvise('layer.item.new', this.path)
     },
     removeItem(index) {
       this.$composer.state.improvise(
@@ -58,6 +60,9 @@ export default {
   computed: {
     value() {
       return this.layer.get(this.path.partial('child'))
+    },
+    spaceLeft() {
+      return this.value.length < this.param.maxLength
     }
   }
 }
@@ -67,10 +72,10 @@ export default {
 <style lang="sass">
 
 $indent-width: 1.125rem
-$indent-padding: 0.125rem
+$indent-padding: 0.25rem
 
 .composer-input-list
-  list-style: decimal
+  counter-reset: index
 
   > .composer-input-title
     display: flex
@@ -94,13 +99,40 @@ $indent-padding: 0.125rem
 
     vertical-align: top
 
+    &:disabled
+      cursor: not-allowed
+      pointer-events: none
+
+      > svg
+        opacity: 0.25
+
+
   &-item
     position: relative
-    margin-left: $indent-width + $indent-padding
+    padding-left: $indent-width + $indent-padding
+
+    counter-increment: index
+
+    background-image: single-color-image(var(--c-fg-hint))
+    background-size: 0.125rem calc(100% - $indent-width * 2 - 0.5rem)
+    background-position: ($indent-width - 0.125rem) / 2 $indent-width + 0.25rem
+    background-repeat: no-repeat
+
+    &::before
+      display: inline-block
+      content: counter(index)
+
+      position: absolute
+      top: 0
+      left: 0
+
+      width: $indent-width
+      text-align: center
 
   &-item > &-button
     position: absolute
     bottom: 0
-    left: -$indent-width - $indent-padding
+    left: 0
+
 
 </style>
