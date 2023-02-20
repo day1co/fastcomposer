81b3de48068cc4d07e067ac8c0ced2c20480f06e
jongyi kim (hibiya) <jikim@day1company.co.kr>
2023-02-20 14:01:46 +0900

    build: change raw-plugin again

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteRawPlugin from 'vite-plugin-raw'
+import string from 'vite-plugin-string'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    viteRawPlugin({
-      match: /\.(ejs|svg)$/i
-    })
+    string({
+      include: [
+        '**/*.ejs',
+        '**/*.svg'
+      ],
+      // exclude: 'node_modules/**',
+      compress(payload) {
+        return payload.replaceAll(/\s+/g, ' ')
+      }
+    }),
+    vue()
   ],
   resolve: {
     alias: {
