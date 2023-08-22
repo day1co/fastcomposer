fc32bee6978a54810e8dcf068c8946ef734cb54e
jongyi kim (hibiya) <jikim@day1company.co.kr>
2023-08-22 17:11:12 +0900

    revert test-purpose flag

--- src/views/composer.vue
+++ src/views/composer.vue
@@ -383,7 +383,7 @@
         },
         isDevicePreviewMode: false,
         deviceType: 'desktop',
-        showConfigWindow: true,
+        showConfigWindow: false,
       };
     },
     methods: {
