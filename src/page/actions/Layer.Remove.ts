e1442f26ee0e592eb3176751babf631df19d6c43
jongyi kim (hibiya) <jikim@day1company.co.kr>
2025-03-14 13:57:06 +0900

    page: Layer.Remove: support multiple paths to be removed

--- src/page/actions/Layer.Remove.ts
+++ src/page/actions/Layer.Remove.ts
@@ -1,34 +1,56 @@
 import type Action from '../../state/action'
 import type Page from '..'
+import type Path from '../path'
 
-import Path from '../path'
+import { Paths } from '../path'
 
 import icon from './icons/Layer.Remove.svg?raw'
 
-export default <Action<Page, Path>>{
+export default <Action<Page, Path | Paths>>{
   id: 'layer.remove',
   title: '레이어 삭제',
   icon,
   perform(root, self, act) {
     const path = act.target
+    const paths = path.type === 'path' ? [path] : path.paths
 
-    const oldLayer = self.getLayerByPath(path)
-    if(!oldLayer)
-      throw new Error(`layer requested to removal couldn't be found (${path.toString()})`)
+    const sortedPaths = [...paths].sort((a, b) => {
+      const indexA = self.pathToIndex(a) ?? 0
+      const indexB = self.pathToIndex(b) ?? 0
+      return indexB - indexA
+    })
 
-    const removedIndex = self.removeLayer(path) ?? 0
-    // let moveFocusTo: Path | undefined
-    // if(self.state.length > removedIndex)
-    //   moveFocusTo = self.state[removedIndex - 1]?.path
+    const removedLayers = []
+    const pathsBeforeRemoved = []
 
-    const pathBeforeRemoved = self.indexToPath(removedIndex)
+    act.meta = self.describe(path)
 
-    // self.moveFocus(moveFocusTo)
+    for (const path of sortedPaths) {
+      const oldLayer = self.getLayerByPath(path)
+      if (!oldLayer)
+        throw new Error(`layer requested to removal couldn't be found (${path.toString()})`)
+
+      const removedIndex = self.removeLayer(path) ?? 0
+      const pathBeforeRemoved = self.indexToPath(removedIndex)
+
+      removedLayers.push(oldLayer)
+      pathsBeforeRemoved.push(pathBeforeRemoved)
+    }
+
     self.setFocus()
-    return act.remember({ layer: oldLayer }, pathBeforeRemoved) // , moveFocusTo)
+
+    return act.remember({
+      layers: removedLayers,
+      positions: pathsBeforeRemoved
+    }, path.type === 'path' ? paths[0] : new Paths(paths))
   },
   rollback(root, self, { target, destination, capturedState }) {
+    const { layers, positions } = capturedState
+
+    for (let i = layers.length - 1; i >= 0; i--) {
+      self.restoreLayer(positions[i], layers[i])
+    }
+
     self.setFocus(target)
-    self.restoreLayer(destination!, capturedState.layer)
   }
 }
--- src/page/test/actions/Layer.Remove.spec.ts
+++ src/page/test/actions/Layer.Remove.spec.ts
@@ -4,17 +4,16 @@ import * as setup from '../setup'
 import type Act from '../../../state/act'
 
 import Path from '../../path'
+import { Paths } from '../../path'
 
 describeAction('layer.remove', ['layer.new'], helpers => {
 
-  it('should work: do, undo', () => {
+  it('should work with single layer: do, undo', () => {
     const [ page, state ] = helpers.createState()
 
     let actNew = helpers.createAct('layer.new', null, setup.MinimalLayout.id)
     helpers.mocked.uniqueId.mockReturnValueOnce('a')
     actNew = <Act>state.perform(actNew)
-
-    console.log(page.state)
 
     expect(actNew.destination.layer).toBe('a')
 
@@ -30,10 +29,70 @@ describeAction('layer.remove', ['layer.new'], helpers => {
     ])
   })
 
+  it('should work with multiple layers: do, undo', () => {
+    const [ page, state ] = helpers.createState()
+
+    // Create 3 layers
+    const actA = helpers.createAct('layer.new', null, setup.MinimalLayout.id)
+    helpers.mocked.uniqueId.mockReturnValueOnce('a')
+    state.perform(actA)
+    const pathA = actA.destination
+
+    const actB = helpers.createAct('layer.new', null, setup.MinimalLayout.id)
+    helpers.mocked.uniqueId.mockReturnValueOnce('b')
+    state.perform(actB)
+    const pathB = actB.destination
+
+    const actC = helpers.createAct('layer.new', null, setup.MinimalLayout.id)
+    helpers.mocked.uniqueId.mockReturnValueOnce('c')
+    state.perform(actC)
+    const pathC = actC.destination
+
+    const paths = new Paths([pathA, pathB])
+
+    helpers.checkTimeParadox(state, [
+      function before() {
+        expect(page.state.length).toEqual(3)
+        expect(page.state.map(l => l.id)).toEqual(['a', 'b', 'c'])
+      },
+      helpers.createAct(paths),
+      function after() {
+        expect(page.state.length).toEqual(1)
+        expect(page.state[0].id).toBe('c')
+      }
+    ])
+  })
+
+  it('should maintain correct order when removing multiple non-adjacent layers', () => {
+    const [ page, state ] = helpers.createState()
+
+    // Create 5 layers
+    const layers = ['a', 'b', 'c', 'd', 'e'].map(id => {
+      const act = helpers.createAct('layer.new', null, setup.MinimalLayout.id)
+      helpers.mocked.uniqueId.mockReturnValueOnce(id)
+      state.perform(act)
+      return act.destination
+    })
+
+    // Remove first and fourth layers (a, d)
+    const paths = new Paths([layers[3], layers[0]])
+
+    helpers.checkTimeParadox(state, [
+      function before() {
+        expect(page.state.map(l => l.id)).toEqual(['a', 'b', 'c', 'd', 'e'])
+      },
+      helpers.createAct(paths),
+      function after() {
+        expect(page.state.map(l => l.id)).toEqual(['b', 'c', 'e'])
+      }
+    ])
+  })
+
   it('should throw on nonexistent layer', () => {
     const [ page, state ] = helpers.createState(setup.ListLayout)
 
     expect(() => state.perform(helpers.createAct(new Path('?')))).toThrowError()
+    expect(() => state.perform(helpers.createAct(new Paths([new Path('?')])))).toThrowError()
   })
 
 })
