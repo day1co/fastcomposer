a22d2f32b2bc2b995bf8bd50f828ee801e5cbe5e
jongyi kim (hibiya) <jikim@day1company.co.kr>
2025-04-28 14:50:12 +0900

    bun support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import viteRawPlugin from 'vite-plugin-raw'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   root: __dirname,
