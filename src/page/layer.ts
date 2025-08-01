e80ea94a62b965ef18b326993aa343e0ab33c097
jongyi kim (hibiya) <jikim@day1company.co.kr>
2025-08-01 17:52:21 +0900

    page: fix meta didn't cloned

--- src/page/layer.ts
+++ src/page/layer.ts
@@ -105,7 +105,7 @@ export default class Layer {
   }
 
   clone(id = util.uniqueId()) {
-    return new Layer(id, this.layout, structuredClone(this.values))
+    return new Layer(id, this.layout, structuredClone(this.values), structuredClone(this.meta))
   }
 
   _getDef(key: string) {
